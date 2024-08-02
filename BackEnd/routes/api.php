6db64e94ed47e9c5af07550b475e5eb156db2cc7
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-08-02 15:51:07 +0700

    feat: create api for cart and cart item

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CartItemController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +14,6 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('products', ProductController::class);
 Route::apiResource('users', UserController::class);
+Route::apiResource('carts', CartController::class);
+Route::apiResource('comments', CommentController::class);
+Route::apiResource('cart-items', CartItemController::class);
